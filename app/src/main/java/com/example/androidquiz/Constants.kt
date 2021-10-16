a5a9236d604b3ec0389bcff4414a7ebd864e45f0
Hasan Ali <hasan.ali@lacritz.com>
2021-10-16 21:02:07 +0530

    Added question UI and connected model to the UI

--- app/src/main/java/com/example/androidquiz/Constants.kt
+++ app/src/main/java/com/example/androidquiz/Constants.kt
@@ -34,9 +34,35 @@ object Constants {
             3
         )
 
+        val quest4 = Question(
+            1,
+            "What country does this flag belong to?",
+            R.drawable.ic_flag_of_belgium,
+            "India",
+            "Belgium",
+            "Korea",
+            "USA",
+            3
+        )
+
+        val quest5 = Question(
+            1,
+            "What country does this flag belong to?",
+            R.drawable.ic_flag_of_germany,
+            "India",
+            "Germany",
+            "Arab",
+            "Canada",
+            3
+        )
+
+
+
         questions.add(quest1)
         questions.add(quest2)
         questions.add(quest3)
+        questions.add(quest4)
+        questions.add(quest5)
 
         return questions
     }
--- app/src/main/java/com/example/androidquiz/QuizQuestionsActivity.kt
+++ app/src/main/java/com/example/androidquiz/QuizQuestionsActivity.kt
@@ -3,13 +3,40 @@ package com.example.androidquiz
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_quiz_questions.*
 
 class QuizQuestionsActivity : AppCompatActivity() {
+    var currentPosition: Int = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_questions)
         val questionsList = Constants.getQuestions();
+        assignValues(questionsList)
+        button_ans_submit.setOnClickListener {
+            currentPosition += 1
+            onSubmit(questionsList)
+        }
 
-        Log.i("Questions Size","${questionsList.size}")
+        Log.i("Questions Size", "${questionsList.size}")
+    }
+
+    private fun assignValues(questions: ArrayList<Question>) {
+//        currentPosition = 1;
+        val question: Question = questions[currentPosition - 1]
+        progress_bar.progress = currentPosition
+        progress_bar.max = questions.size
+        text_view_progress.text = "$currentPosition" + "/" + progress_bar.max
+        text_view_question.text = question.question
+        image_view_question_image.setImageResource(question.image)
+        text_view_option_one.text = question.optionOne
+        text_view_option_two.text = question.optionTwo
+        text_view_option_three.text = question.optionThree
+        text_view_option_four.text = question.optionFour
+    }
+
+    private fun onSubmit(questions: ArrayList<Question>) {
+        if (currentPosition > questions.size) return
+        assignValues(questions)
     }
 }
